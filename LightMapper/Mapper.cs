e3b4f3be4b4184c2d88e1b652d0779afbe5507a6
mehrdad <mehrdadab@yahoo.com>
2019-05-11 19:46:01 +1000

    Adding first version of LightMapper to the repository

---
+++ LightMapper/Mapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LightMapper
+{
+    public class Mapper
+    {
+        public Destination Map<Source, Destination>(Source source) where Destination : class, new()
+        {
+            var destination = new Destination();
+            Type destinationType= destination.GetType();
+            Type sourceType = source.GetType();
+            Console.WriteLine(sourceType.Name);
+            Console.WriteLine(destinationType.Name);
+            var propertiesInfo =  sourceType.GetProperties();
+            for(int i=0;i<propertiesInfo.Length;i++)
+            {
+                Console.WriteLine(propertiesInfo[i].Name+"=>"+ propertiesInfo[i].GetValue(source,null));
+
+                destination =  SetValue<Destination>(destination, propertiesInfo[i].Name, propertiesInfo[i].GetValue(source, null),propertiesInfo[i].PropertyType);
+            }
+            destination = (Destination)ProfileRunner.Run(source.GetType(),destination.GetType(),source,destination);
+            return destination;
+        }
+        private Destination SetValue<Destination>(Destination destination,string propertyName,object valueToBeSet,Type sourceType)
+        {
+            Type t = destination.GetType();
+            PropertyInfo[] propertiesInfo = t.GetProperties();
+            //propertiesInfo.SetValue()
+            for (int i = 0; i < propertiesInfo.Length; i++)
+            {   
+                if(propertiesInfo[i].Name == propertyName)
+                {
+                    Type propertyType = propertiesInfo[i].PropertyType;
+                    if(propertyType.Name == sourceType.Name)
+                        propertiesInfo[i].SetValue(destination, valueToBeSet);
+                    
+                    break;
+                }
+                    //+ "=>" + propertiesInfo[i].GetValue(dest, null));
+            }
+            return destination;
+        }
+    }
+}
---
+++ LightMapper/ProfileRunner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LightMapper
+{
+    public class ProfileRunner
+    {
+        public static object Run(Type sourceType,Type DestType,params object[] parameters)
+        {
+            object returnedResult = null;
+            int numberOfMethodsFound = 0;
+            var type = typeof(ILightMapperProfile);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p));
+            foreach (var item in types)
+            {
+                MethodInfo methodInfo = null;
+                try
+                {
+                    methodInfo = item.GetMethods().SingleOrDefault(d => d.GetParameters().Count() == 2 && d.GetParameters()[0].ParameterType == sourceType && d.GetParameters()[1].ParameterType == DestType);
+                }
+                catch
+                {
+                    throw new Exception("Two functions with the same name is not allowed inside the LightMapper profile classes.");
+                }
+                //MethodInfo methodInfo = item.GetMethod(methodName);
+                if (methodInfo != null)
+                {
+                    numberOfMethodsFound++;
+                    if (numberOfMethodsFound > 1)
+                        throw new Exception("Two functions with the same name is not allowed inside the LightMapper profile classes.");
+                    object classInstance = Activator.CreateInstance(item, null);
+                    returnedResult = methodInfo.Invoke(classInstance, parameters);
+                }
+            }
+
+            return returnedResult;
+        }
+    }
+}

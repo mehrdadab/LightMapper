b65a2e261d0c71a7918e69e46344b2ddbea4196a
mehrdad <mehrdadab@yahoo.com>
2019-05-12 19:28:40 +1000

    Cleaning Mapper class

--- LightMapper/Mapper.cs
+++ LightMapper/Mapper.cs
@@ -13,20 +13,29 @@ namespace LightMapper
         }
         public Destination Map<Source, Destination>(Source source) where Destination : class, new()
         {
-            var destination = new Destination();
-            Type destinationType = destination.GetType();
-            Type sourceType = source.GetType();
-            var propertiesInfo = sourceType.GetProperties();
-            string ignoreKey = NameCreator.IgnoreKey(typeof(Source), typeof(Destination));
             string[] ignoreList = null;
             bool isAnyItemIgnored = false;
+
+            var destination = new Destination();
+
+            Type destinationType = destination.GetType();
+
+            Type sourceType = source.GetType();
+
+            var propertiesInfo = sourceType.GetProperties();
+
+            string ignoreKey = NameCreator.IgnoreKey(typeof(Source), typeof(Destination));
+
             if (MapperCore.IgnoreList!=null)
             {
                 isAnyItemIgnored = MapperCore.IgnoreList.TryGetValue(ignoreKey, out ignoreList);
             }
+
             for (int i = 0; i < propertiesInfo.Length; i++)
             {
-              bool  isCurrentItemIgnored = false;
+
+                bool  isCurrentItemIgnored = false;
+
                 if (isAnyItemIgnored && ignoreList != null && ignoreList.Length > 0)
                 {
                     for (int j = 0; j < ignoreList.Length; j++)
@@ -42,25 +51,30 @@ namespace LightMapper
                 if (isCurrentItemIgnored == false)
                     destination = SetValue<Destination>(destination, propertiesInfo[i].Name, propertiesInfo[i].GetValue(source, null), propertiesInfo[i].PropertyType);
             }
+
             destination = (Destination)ProfileRunner.Run(source.GetType(), destination.GetType(), source, destination);
+
             return destination;
         }
         private Destination SetValue<Destination>(Destination destination, string propertyName, object valueToBeSet, Type sourceType)
         {
             Type t = destination.GetType();
+
             PropertyInfo[] propertiesInfo = t.GetProperties();
-            //propertiesInfo.SetValue()
+
             for (int i = 0; i < propertiesInfo.Length; i++)
             {
                 if (propertiesInfo[i].Name == propertyName)
                 {
                     Type propertyType = propertiesInfo[i].PropertyType;
+
                     if (propertyType.Name == sourceType.Name)
                         propertiesInfo[i].SetValue(destination, valueToBeSet);
 
                     break;
                 }
             }
+
             return destination;
         }
     }

088439599def3a28538ec19d92052e83129455a9
mehrdad <mehrdadab@yahoo.com>
2019-05-12 14:01:31 +1000

    Creating a class to create a string based on the types, used to create unique keys

---
+++ LightMapper/NameCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightMapper
+{
+    public class NameCreator
+    {
+        public static string IgnoreKey(Type source,Type destination)
+        {
+            string sourceName = source.FullName;
+            string destinationName = destination.FullName;
+            return $"{sourceName}-{destinationName}";
+
+        }
+    }
+}

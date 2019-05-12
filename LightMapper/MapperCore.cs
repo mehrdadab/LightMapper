6b977fda9075398ad42300e64851d94986c6eabe
mehrdad <mehrdadab@yahoo.com>
2019-05-12 14:02:32 +1000

    Creating a core class to keep data

---
+++ LightMapper/MapperCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightMapper
+{
+    public class MapperCore
+    {
+        public static ConcurrentDictionary<string, string[]> IgnoreList { get; set; }
+    }
+}

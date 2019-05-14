0e0383212e217e162a22a85c5eed717db563ebf5
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-15 08:44:31 +1000

    Refactoring Mapper class

--- LightMapper/Mapper.cs
+++ LightMapper/Mapper.cs
@@ -24,38 +24,51 @@ namespace LightMapper
 
             var propertiesInfo = sourceType.GetProperties();
 
-            string ignoreKey = NameCreator.CacheKey(typeof(Source), typeof(Destination));
-
-            if (MapperCore.IgnoreList!=null)
-            {
-                isAnyItemIgnored = MapperCore.IgnoreList.TryGetValue(ignoreKey, out ignoreList);
-            }
+            GetIgnoreList<Source, Destination>(ref ignoreList, ref isAnyItemIgnored);
 
             for (int i = 0; i < propertiesInfo.Length; i++)
             {
 
-                bool  isCurrentItemIgnored = false;
+                bool isCurrentItemIgnored = false;
 
-                if (isAnyItemIgnored && ignoreList != null && ignoreList.Length > 0)
-                {
-                    for (int j = 0; j < ignoreList.Length; j++)
-                    {
-                        if (ignoreList[j] == propertiesInfo[i].Name)
-                        {
-                            isCurrentItemIgnored = true;
-                            break;
-                        }
-
-                    }
-                }
+                isCurrentItemIgnored = IsItemIgnored(ignoreList, isAnyItemIgnored, propertiesInfo, i, isCurrentItemIgnored);
                 if (isCurrentItemIgnored == false)
                     destination = SetValue<Destination>(destination, propertiesInfo[i].Name, propertiesInfo[i].GetValue(source, null), propertiesInfo[i].PropertyType);
             }
 
-          destination = (Destination)ProfileRunner.Run(source.GetType(), destination.GetType(), source, destination);
+            destination = (Destination)ProfileRunner.Run(source.GetType(), destination.GetType(), source, destination);
 
             return destination;
         }
+
+        private bool IsItemIgnored(string[] ignoreList, bool isAnyItemIgnored, PropertyInfo[] propertiesInfo, int i, bool isCurrentItemIgnored)
+        {
+            if (isAnyItemIgnored && ignoreList != null && ignoreList.Length > 0)
+            {
+                for (int j = 0; j < ignoreList.Length; j++)
+                {
+                    if (ignoreList[j] == propertiesInfo[i].Name)
+                    {
+                        isCurrentItemIgnored = true;
+                        break;
+                    }
+
+                }
+            }
+
+            return isCurrentItemIgnored;
+        }
+
+        private void GetIgnoreList<Source, Destination>(ref string[] ignoreList, ref bool isAnyItemIgnored) where Destination : class, new()
+        {
+            string ignoreKey = NameCreator.CacheKey(typeof(Source), typeof(Destination));
+
+            if (MapperCore.IgnoreList != null)
+            {
+                isAnyItemIgnored = MapperCore.IgnoreList.TryGetValue(ignoreKey, out ignoreList);
+            }
+        }
+
         private Destination SetValue<Destination>(Destination destination, string propertyName, object valueToBeSet, Type sourceType)
         {
             Type t = destination.GetType();

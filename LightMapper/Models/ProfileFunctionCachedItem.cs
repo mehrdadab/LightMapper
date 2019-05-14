5fe9120098ce3a0d28ecc41b94786403715ca98c
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-14 11:20:11 +1000

    Adding a model to keep function info while running a profile function for the first time

---
+++ LightMapper/Models/ProfileFunctionCachedItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightMapper.Models
+{
+    public class ProfileFunctionCachedItem
+    {
+        public Type Source { get; set; }
+        public Type Destination { get; set; }
+    }
+}

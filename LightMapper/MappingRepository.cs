d6cdac0d1fe08442478c1d54a31280ef12fa1a0b
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-17 14:28:47 +1000

    Renaming a class responsible about keeping mapping delegates

---
+++ LightMapper/MappingRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LightMapper
+{
+    public class MappingRepository<Source, Destination, Value> : IMappingRepository<Source, Destination> where Source : class where Destination : class
+    {
+        private Func<Source, Value> getter = null;
+        private Action<Destination, Value> setter = null;
+        public void Map(Source source, Destination destination, string propertyName = null)
+        {
+             if (getter == null)
+            {
+                MethodInfo methodInfoGet = typeof(Source).GetProperty(propertyName).GetGetMethod();
+                getter = (Func<Source, Value>)Delegate.CreateDelegate(typeof(Func<Source, Value>), null, methodInfoGet);
+            
+                MethodInfo methodInfoSet = typeof(Destination).GetProperty(propertyName).GetSetMethod();
+                setter = (Action<Destination, Value>)Delegate.CreateDelegate(typeof(Action<Destination, Value>), null, methodInfoSet);
+            }
+
+            var val = getter(source);
+            setter(destination, val);
+        }
+    }
+}

be46a6c38fa2bfc4916dfe687694b9aaa6028a02
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-20 17:16:35 +1000

    Checking propertyname before creating cachekey

--- LightMapper/NameCreator.cs
+++ LightMapper/NameCreator.cs
@@ -15,6 +15,8 @@ namespace LightMapper
         }
         public static string CacheKey(Type source, Type destination,string propertyName)
         {
+            if (String.IsNullOrWhiteSpace(propertyName))
+                throw new ArgumentNullException("propertyName", "propertyName must have a value");
             string sourceName = source.FullName;
             string destinationName = destination.FullName;
             return $"{sourceName}-{destinationName}-{propertyName}";

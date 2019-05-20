c302e3beca2edc97184ad467bb4e8656d3bf6aab
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-20 17:15:47 +1000

    Adding Test Project

---
+++ LightMapperTest/NameCreatorTest.cs
@@ -0,0 +1,22 @@
+using LightMapper;
+using System;
+using Xunit;
+
+namespace LightMapperTest
+{
+    public class NameCreatorTest
+    {
+        [Fact]
+        public void Create_CacheKey_withTwoParameters()
+        {
+            string cacheKey = NameCreator.CacheKey(typeof(string), typeof(Int32));
+            Assert.Equal("System.String-System.Int32", cacheKey);
+        }
+        [Fact]
+        public void Create_CacheKey_withThreeParameters()
+        {
+            string cacheKey = NameCreator.CacheKey(typeof(string), typeof(Int32),"PropertyName");
+            Assert.Equal("System.String-System.Int32-PropertyName", cacheKey);
+        }
+    }
+}

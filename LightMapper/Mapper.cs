fe9be562a61b694bbd2eadc84c715af0e6eb2543
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-20 11:21:36 +1000

    Finalising running profile by delegate

--- LightMapper/Mapper.cs
+++ LightMapper/Mapper.cs
@@ -16,52 +16,13 @@ namespace LightMapper
         }
         public Destination Map<Source, Destination>(Source source)where Source:class where Destination : class, new()
         {
-            //string[] ignoreList = null;
-            //bool isAnyItemIgnored = false;
-            //PropertyInfo destPropertyInfo = null;
+             var dest = SetValues<Source, Destination>(source);
 
-            //var destination = new Destination();
-
-            //Type destinationType = destination.GetType();
-
-            //Type sourceType = source.GetType();
-
-            //var propertiesInfo = sourceType.GetProperties();
-            var dest = SetValues<Source, Destination>(source);
             var func = ProfileDelegateCreator.CreateDelegate<Source, Destination>();
-            if(func!=null)
+
+            if (func!=null)
             dest = func(source, dest);
-            //CachedObject[] objectInfoList = ObjectCache.GetObject(sourceType, destinationType);
-            //if (objectInfoList == null)
-            //{
-            //    isAnyItemIgnored = GetIgnoreList(typeof(Source), typeof(Destination),out ignoreList);
 
-            //    IList<CachedObject> tempObjectList = new List<CachedObject>();
-            //    for (int i = 0; i < propertiesInfo.Length; i++)
-            //    {
-            //        bool isCurrentItemIgnored = IsItemIgnored(ignoreList, isAnyItemIgnored, propertiesInfo, i);
-
-            //        if (isCurrentItemIgnored == false)
-            //        {
-            //            destination = SetValue<Destination>(destination, propertiesInfo[i].Name, propertiesInfo[i].GetValue(source, null), propertiesInfo[i].PropertyType, out destPropertyInfo);
-
-            //            tempObjectList.Add(new CachedObject {SourceObjectInfo=propertiesInfo[i],DestinationObjectInfo=destPropertyInfo });
-
-            //        }
-            //    }
-
-            //    ObjectCache.SetObject(sourceType, destinationType, tempObjectList.ToArray());
-            //    destination = (Destination)ProfileRunner.Run(source.GetType(), destination.GetType(), source, destination);
-            //}
-            //else
-            //{
-            //    for(int i=0;i<objectInfoList.Length;i++)
-            //    {
-            //        object valueToBeSet = objectInfoList[i].SourceObjectInfo.GetValue(source, null);
-
-            //        objectInfoList[i].DestinationObjectInfo.SetValue(destination, valueToBeSet);
-            //    }
-            //}
             return dest;
         }
 
@@ -99,44 +60,17 @@ namespace LightMapper
             return isAnyItemIgnored;
         }
 
-        private Destination SetValue<Destination>(Destination destination, string propertyName, object valueToBeSet, Type sourceType, out PropertyInfo destPropertyInfo)
-        {
-            destPropertyInfo = null;
-            Type t = destination.GetType();
-
-            PropertyInfo[] propertiesInfo = t.GetProperties();
-
-            for (int i = 0; i < propertiesInfo.Length; i++)
-            {
-                if (propertiesInfo[i].Name == propertyName)
-                {
-                    Type propertyType = propertiesInfo[i].PropertyType;
-
-                    if (propertyType.Name == sourceType.Name)
-                    {
-                        propertiesInfo[i].SetValue(destination, valueToBeSet);
-
-                        destPropertyInfo = propertiesInfo[i];
-                    }
-                    else
-                    {
-                        destPropertyInfo = null;
-                    }
-
-                    break;
-                }
-            }
-
-            return destination;
-        }
+     
         private Destination SetValues<Source, Destination>(Source source) where Source:class where Destination : class, new()
         {
             var cacheKey = "";
-            //var cacheKey = NameCreator.CacheKey(typeof(Source), typeof(Destination));
-            //IGenericGetterSetter<Source, Destination> cachedObject = null;
+
             object[] cachedObject = null;
+
             Destination destination =  new Destination();
+
             MapperCore.MapDelegateList.TryGetValue(cacheKey, out cachedObject);
+
             if (cachedObject != null)
             {
                 for (int i = 0; i < cachedObject.Length; i++)
@@ -147,20 +81,26 @@ namespace LightMapper
             else
             {
                 Type sourceType = source.GetType();
+
                 PropertyInfo[] properties = sourceType.GetProperties();
+
                 IList<object> listOfDelegates = new List<object>();
+
                 foreach (var item in properties)
                 {
 
                     Type genericClass = typeof(MappingRepository<,,>);
+
                     Type constructedClass = genericClass.MakeGenericType(typeof(Source), typeof(Destination), item.PropertyType);
+
                     IMappingRepository<Source,Destination> created = (IMappingRepository<Source, Destination>)Activator.CreateInstance(constructedClass);
+
                     created.Map(source, destination, item.Name);
-                    //var test = new MappingRepository<Source, Destination, object>();
-                    //test.Map(source, destination,item.Name);
+
                     listOfDelegates.Add(created);
                 }
-                 MapperCore.MapDelegateList.TryAdd(cacheKey, listOfDelegates.ToArray());
+
+                MapperCore.MapDelegateList.TryAdd(cacheKey, listOfDelegates.ToArray());
             }
 
 

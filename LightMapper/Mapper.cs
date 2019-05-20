357d6f8bdb7bc910920e97c803c4ab3b2542a127
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-20 12:45:45 +1000

    Refactoring Mapper

--- LightMapper/Mapper.cs
+++ LightMapper/Mapper.cs
@@ -26,41 +26,6 @@ namespace LightMapper
             return dest;
         }
 
-        private bool IsItemIgnored(string[] ignoreList, bool isAnyItemIgnored, PropertyInfo[] propertiesInfo, int i)
-        {
-            bool isCurrentItemIgnored = false;
-
-            if (isAnyItemIgnored && ignoreList != null && ignoreList.Length > 0)
-            {
-                for (int j = 0; j < ignoreList.Length; j++)
-                {
-                    if (ignoreList[j] == propertiesInfo[i].Name)
-                    {
-                        isCurrentItemIgnored = true;
-                        break;
-                    }
-
-                }
-            }
-
-            return isCurrentItemIgnored;
-        }
-
-        private bool GetIgnoreList(Type sourceType, Type destinationType, out string[] ignoreList)
-        {
-            ignoreList = null;
-            bool isAnyItemIgnored = false;
-
-            string ignoreKey = NameCreator.CacheKey(sourceType, destinationType);
-
-            if (MapperCore.IgnoreList != null)
-            {
-                isAnyItemIgnored = MapperCore.IgnoreList.TryGetValue(ignoreKey, out ignoreList);
-            }
-            return isAnyItemIgnored;
-        }
-
-
         private Destination SetValues<Source, Destination>(Source source) where Source : class where Destination : class, new()
         {
             var cacheKey = "";
@@ -80,31 +45,40 @@ namespace LightMapper
             }
             else
             {
-                Type sourceType = source.GetType();
-
-                PropertyInfo[] properties = sourceType.GetProperties();
-
-                IList<object> listOfDelegates = new List<object>();
-
-                foreach (var item in properties)
-                {
-
-                    Type genericClass = typeof(MappingRepository<,,>);
-
-                    Type constructedClass = genericClass.MakeGenericType(typeof(Source), typeof(Destination), item.PropertyType);
-
-                    IMappingRepository<Source, Destination> created = (IMappingRepository<Source, Destination>)Activator.CreateInstance(constructedClass);
-
-                    created.Map(source, destination, item.Name);
-
-                    listOfDelegates.Add(created);
-                }
+                IList<object> listOfDelegates = CreateMappingRepository(source, destination);
 
                 MapperCore.MapDelegateList.TryAdd(cacheKey, listOfDelegates.ToArray());
             }
 
 
             return destination;
+        }
+
+        private static IList<object> CreateMappingRepository<Source, Destination>(Source source, Destination destination)
+            where Source : class
+            where Destination : class, new()
+        {
+            Type sourceType = source.GetType();
+
+            PropertyInfo[] properties = sourceType.GetProperties();
+
+            IList<object> listOfDelegates = new List<object>();
+
+            foreach (var item in properties)
+            {
+
+                Type genericClass = typeof(MappingRepository<,,>);
+
+                Type constructedClass = genericClass.MakeGenericType(typeof(Source), typeof(Destination), item.PropertyType);
+
+                IMappingRepository<Source, Destination> created = (IMappingRepository<Source, Destination>)Activator.CreateInstance(constructedClass);
+
+                created.Map(source, destination, item.Name);
+
+                listOfDelegates.Add(created);
+            }
+
+            return listOfDelegates;
         }
     }
 }

42ddf64d3a2ef9e5f59e7487301d710692ec5446
Mehrdad Babaki <mehrdadb@adept.com.au>
2019-05-21 09:12:24 +1000

    Test:Adding new test for name creator

--- LightMapperTest/NameCreatorTest.cs
+++ LightMapperTest/NameCreatorTest.cs
@@ -15,8 +15,15 @@ namespace LightMapperTest
         [Fact]
         public void Create_CacheKey_withThreeParameters()
         {
-            string cacheKey = NameCreator.CacheKey(typeof(string), typeof(Int32),"PropertyName");
+            string cacheKey = NameCreator.CacheKey(typeof(string), typeof(Int32), "PropertyName");
             Assert.Equal("System.String-System.Int32-PropertyName", cacheKey);
+        }
+        [Fact]
+        public void Create_CacheKey_withPropertyNameEmpty()
+        {
+            const string EMPTY_PROPERTY_NAME = "";
+            Action actual = () => NameCreator.CacheKey(typeof(string), typeof(Int32), EMPTY_PROPERTY_NAME);
+            Assert.Throws<ArgumentNullException>(actual);
         }
     }
 }
